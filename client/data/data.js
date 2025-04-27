0e06b4fa5a5f474d33ec51911cbd43e0300e8a98
iamrosan <roshanshrestha21.63@gmail.com>
2025-04-27 09:46:28 +0545

    feat: initial project setup

---
+++ client/data/data.js
@@ -0,0 +1,16 @@
+export default chat = {
+  user: {
+    details: {
+      timeStamp: "2023-10-01T12:00:00Z",
+      msg: "Hello, how are you?",
+      msgType: "text",
+    },
+  },
+  chatbot: {
+    details: {
+      timeStamp: "2023-10-01T12:00:00Z",
+      msg: "Hello! How can I help you today?",
+      msgType: "text",
+    },
+  },
+};

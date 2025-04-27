48afc2179a1ae7f50a1e6a24ceaf0f68e2bb83fc
iamrosan <roshanshrestha21.63@gmail.com>
2025-04-27 12:35:46 +0545

    chore: initialize chat component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,15 @@
+import Chat from "@/components/Chat/Chat";
 import Dropzone from "@/components/Dropzone/Dropzone";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Dropzone />
+    <div className="flex flex-col h-[80vh] gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="flex flex-row items-center gap-8">
+        <Dropzone />
+        {/* Vertical Line */}
+        <div className="w-[2px] h-[60vh] bg-gray-300" />
+        <Chat />
+      </div>
     </div>
   );
 }
